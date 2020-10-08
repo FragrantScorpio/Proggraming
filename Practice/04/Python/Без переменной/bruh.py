8941c08d28b103e32a478c18751f73f5147143e4
FragrantScorpio <71697086+FragrantScorpio@users.noreply.github.com>
2020-10-09 00:30:43 +0300

    Add files via upload

---
+++ Practice/04/Python/Без переменной/bruh.py
@@ -0,0 +1,8 @@
+print('Введите числа для обмена')
+#int(a,b)
+a=input()
+b=input()
+a = b + a
+b = a - b
+a = a - b
+print(a,b)
---
+++ Practice/04/Python/С переменной/change.py
@@ -0,0 +1,8 @@
+#int(a,b,c)
+print("Введите числа для обмена")
+a=input()
+b=input()
+c=a
+a=b
+b=c
+print(a,b)
