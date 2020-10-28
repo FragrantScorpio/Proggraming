f70a6963f88b21bdb6e9f01fa5a710b0010aeb7a
FragrantScorpio <71697086+FragrantScorpio@users.noreply.github.com>
2020-10-28 21:28:09 +0300

    Update feic.cpp

--- Practice/12/C++/feic.cpp
+++ Practice/12/C++/feic.cpp
@@ -1,11 +1,16 @@
-﻿#include<iostream>;
+#include<iostream>;
 using namespace std;
-int factor, number, i,x,y;
+int factor, number, i, x, y;
 int main() {
+	setlocale(LC_CTYPE, "rus");
 	factor = 1;
 	cout << "Введите число для факториала";
 	cin >> number;
-	for(i=0;  i<number; i++)
+	if (number < 0) {
+		cout << "Число должно быть положительным";
+		return(0);
+	}
+	for (i = 0; i < number; i++)
 	{
 		x = x + 1;
 		factor = factor * x;
