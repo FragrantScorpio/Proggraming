646bdaeef6dda4624aacf1842221ac20c9e249ff
FragrantScorpio <71697086+FragrantScorpio@users.noreply.github.com>
2020-10-28 19:37:52 +0300

    Add files via upload

---
+++ Practice/12/C++/feic.cpp
@@ -0,0 +1,14 @@
+﻿#include<iostream>;
+using namespace std;
+int factor, number, i,x,y;
+int main() {
+	factor = 1;
+	cout << "Введите число для факториала";
+	cin >> number;
+	for(i=0;  i<number; i++)
+	{
+		x = x + 1;
+		factor = factor * x;
+	}
+	cout << factor;
+}
