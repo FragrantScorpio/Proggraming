68a05c85ee8b4469f427f041674463fbafb44e62
FragrantScorpio <71697086+FragrantScorpio@users.noreply.github.com>
2020-10-30 01:07:40 +0300

    Update FixPrice.py

--- Practice/10/Python/FixPrice.py
+++ Practice/10/Python/FixPrice.py
@@ -1,25 +1,18 @@
-print("Введите число и диапазоны")
+print("Введите число и диапазоны через пробел")
+s, l1, r1, l2, r2 = input().split(' ')
 z = 0
-s = int(input())
-l1 = int(input())
-r1 = int(input()) 
-l2 = int(input()) 
-r2 = int(input())  
-if (l1-l2)==s:
+s = int (s)
+l1 = int (l1)
+r1 = int (r1)   
+r2 = int (r2)   
+l2 = int (l2)     
+if (l1-l2)==s or (l1+l2)==s:
     print(l1,l2)
-elif (l1-r2)==s:
+elif (l1-r2)==s or (l1+r2)==s:
     print(l1,r2)
-elif (r1-l2)==s:
+elif (r1-l2)==s or (r1+l2)==s:
     print(r1-l2)
-elif (r1-r2)==s:
-    print(r1,r2)
-elif (l1+l2)==s:
-    print(l1,l2)
-elif (l1+r2)==s:
-    print(l1,r2)
-elif (r1+l2)==s:
-    print(r1-l2)
-elif (r1+r2)==s:
+elif (r1-r2)==s or (r1+r2)==s:
     print(r1,r2)
 else:
     print(-1)
